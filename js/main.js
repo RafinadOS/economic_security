8807b5b14a248690866b1d5cd03b5af423c40533
Павел Рафеев <rafeev@office.bitrix.ru>
2017-03-22 17:51:00 +0200

    add responive for content

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
-$(document).ready(function() {
+window.onload = function(){
+	
+	function setTwitterStyle(id){
+		var frameContent = document.getElementById(id).contentWindow.document;
+		var frameBlockBg = frameContent.querySelector('.timeline-Widget');
+		frameBlockBg.style.background = 'transparent';
+	}
 
-
-
-});
+	setTwitterStyle('twitter-widget-0');
+}
