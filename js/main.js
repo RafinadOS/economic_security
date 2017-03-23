196f53c2f7c8db5608875d42e79b60fa15678ff4
Павел Рафеев <rafeev@office.bitrix.ru>
2017-03-23 17:12:52 +0200

    slide menu add

--- js/main.js
+++ js/main.js
@@ -1,10 +1,36 @@
 window.onload = function(){
 	
-	function setTwitterStyle(id){
+	var bodyWidth = document.body.offsetWidth;
+
+	function setTwitterStyle(id) {
 		var frameContent = document.getElementById(id).contentWindow.document;
-		var frameBlockBg = frameContent.querySelector('.timeline-Widget');
-		frameBlockBg.style.background = 'transparent';
+		if (frameContent) 
+		{
+			var frameBlockBg = frameContent.querySelector('.timeline-Widget');
+			frameBlockBg.style.background = 'transparent';
+		}
 	}
 
-	setTwitterStyle('twitter-widget-0');
+	function slideMenu() {
+
+		var buttonToggle = document.querySelector('.show-menu');
+		var body = document.querySelector('.body');
+
+		buttonToggle.addEventListener('click', function(){
+			body.classList.toggle('left-menu-open');
+		}, false);
+
+		if(bodyWidth > 740)
+		{
+			body.classList.remove('left-menu-open');
+		}
+
+	}
+
+	if(bodyWidth <= 740) {
+		slideMenu()
+	}
+
+	setTwitterStyle('twitter-widget-0'), 500;
+	window.addEventListener('resize', slideMenu, false);
 }
